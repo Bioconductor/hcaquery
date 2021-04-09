54cc2bdfa2953467859188a8564536059bbb67ca
Maya Reed McDaniel <maya.mcdaniel@roswellpark.org>
2021-04-09 17:38:35 -0400

    adding functions

---
+++ R/database.R
@@ -0,0 +1,134 @@
+#' @title Populating database with data output of single cell experiments
+#'
+#' @rdname database
+#'
+#' @param file_tbl tbl_hca tibble of files as returned by hca::files()
+#'
+#' @export
+files_to_db <- function(file_tbl = NULL) {
+    stopifnot(
+        ## input must be of class tbl_hca
+        inherits(file_tbl, "tbl_hca"),
+        ## must be a tbl_hca of files
+        `'file_tbl =' must contain columns "url", "name", "fileID` =
+            all(c("fileId", "url", "name") %in% names(file_tbl))
+    )
+
+    ## download files and return location of files
+    file_locations <- files_download(file_tbl)
+    file_tbl_aug <- file_tbl %>%
+        tibble::add_column(file_locations = file_locations)
+
+    ## generate a tibble of file locations and associated projectIds
+    project_titles <- file_tbl_aug$projectTitle
+    file_filter <- filters(projectTitle = list(is = project_titles))
+    test_proj <- projects(file_filter)
+    file_and_projIds <- file_tbl_aug %>%
+        left_join(test_proj, by = "projectTitle") %>%
+        select(fileId, name, projectTitle, file_locations, projectId)
+
+    ## apply to each pair of file path and project ID
+    mapply(
+        .single_file_to_db,
+        file_and_projIds$file_locations,
+        file_and_projIds$fileId,
+        file_and_projIds$projectTitle,
+        file_and_projIds$projectId
+    )
+}
+
+#' @rdname database
+#'
+#' @param file_path character() location of experiment output file
+#' @param fileID character() unique identifier of file
+#' @param projectTitle character() title of file's associated project
+#' @param projectId character() unique identifier of
+#' the file's associated project
+.single_file_to_db <- function(file_path, fileId, projectTitle, projectId) {
+    stopifnot(
+        ## file_path must be a non-null character vector
+        `'file_path =' must be a non-null character vector` =
+            hca:::.is_scalar_character(file_path),
+        ## fileId must be a non-null character vector
+        `'fileId =' must be a non-null character vector` =
+            hca:::.is_scalar_character(fileId)
+    )
+    ## connect to database
+    con <- DBI::dbConnect(RPostgres::Postgres(),
+                          host = "localhost",
+                          dbname = "bioc_hca",
+                          user = "hca_user",
+                          port = 5432,
+                          password = rstudioapi::askForPassword("Database password")
+    )
+
+    ## first, check to see if file already exists in the database as not to
+    ## duplicate data
+    overview_table_exists <- con %>% DBI::dbExistsTable("experiment_overviews")
+    file_exists_in_db <- FALSE
+
+    if(overview_table_exists){
+        existing_experiments_tbl <- con %>% tbl("experiment_overviews")
+        files_available <- existing_experiments_tbl$fileId
+        if(!is.null(files_available) && fileID %in% files_available){
+            file_exists_in_db <- TRUE
+        }
+    } else {
+        existing_experiments_tbl <- tibble(fileId = character(),
+                                           projectId = character(),
+                                           projectTitle = character())
+    }
+
+    ## if file does not already exist in the database, proceed with adding it
+    if(!file_exists_in_db){
+        file_ext <- tools::file_ext(file_path)
+        sce <- switch(file_ext,
+                      "loom" = .loom_to_sce(file_path),
+                      "h5ad" = .h5ad_to_sce(file_path))
+        sparse_matrix <- switch(file_ext,
+                                "loom" = .loom_sparse_matrix(file_path),
+                                "h5ad" = .h5ad_sparse_matrix(file_path))
+
+        assay_tbl <- switch(file_ext,
+                            "loom" = .loom_sparse_mtx_to_assay_tbl(sparse_matrix),
+                            "h5ad" = .h5ad_sparse_mtx_to_assay_tbl(sparse_matrix))
+
+        gene_tbl <- .sce_rowdata_to_gene_tbl(sce)
+
+        cell_tbl <- .sce_coldata_to_cell_tbl(sce)
+
+        ## if any column in any table is of type "raw" i.e. byte data
+        ## conversion is needed
+        assay_tbl_recast <- assay_tbl %>%
+            #mutate(across(where(is.raw), ~ rawToChar(.x, multiple = T)))
+            mutate(across(where(is.raw), as.logical))
+
+        gene_tbl_recast <- gene_tbl %>%
+            #mutate(across(where(is.raw), ~ rawToChar(.x, multiple = T)))
+            mutate(across(where(is.raw), as.logical))
+
+        cell_tbl_recast <- cell_tbl %>%
+            #mutate(across(where(is.raw), ~ rawToChar(.x, multiple = T)))
+            mutate(across(where(is.raw), as.logical))
+
+        ## figure out how we want to name tables
+        dplyr::copy_to(con, assay_tbl_recast,
+                       paste(c(fileId, "assay"), collapse = "_"),
+                       temporary = FALSE)
+        dplyr::copy_to(con, gene_tbl_recast,
+                       paste(c(fileId, "gene"), collapse = "_"),
+                       temporary = FALSE)
+        dplyr::copy_to(con, cell_tbl_recast,
+                       paste(c(fileId, "cell"), collapse = "_"),
+                       temporary = FALSE)
+
+        ## add details to overview table
+        existing_experiments_tbl %>% add_row(fileId = fileId,
+                                             projectId = projectId,
+                                             projectTitle = projectTitle)
+        dplyr::copy_to(con, existing_experiments_tbl,
+                       name = "experiment_overviews",
+                       temporary = FALSE,
+                       overwrite = TRUE)
+    }
+}
---
+++ R/sce.R
@@ -0,0 +1,175 @@
+#' @title Helper functions for the processing of .loom and .h5ad files into
+#' relational database tables
+#'
+#' @rdname sce
+#'
+#' @param loom_filepath character() absolute path to downloaded .loom file
+#'
+#' @return SingleCellLoomExperiment object
+.loom_to_sce <- function(loom_filepath) {
+    loom_sce <- LoomExperiment::import(loom_filepath,
+                                       type ="SingleCellLoomExperiment")
+    loom_sce
+}
+
+#' @rdname sce
+#'
+#' @param h5ad_filepath character() absolute path to downloaded .h5ad file
+#'
+#' @return SingleCellExperiment object
+.h5ad_to_sce <- function(h5ad_filepath) {
+    h5ad_sce <- zellkonverter::readH5AD(h5ad_filepath)
+    h5ad_sce
+}
+
+#' @rdname sce
+#'
+#' @description  function to change SingleCellLoomExperiment object's assa
+#'  matrix into sparse representation (`dgCMatrix`)
+#'
+#' @param loom_filepath character() absolute path to downloaded .loom file
+#'
+#' @return dgCMatrix sparse matrix representation
+.loom_sparse_matrix <- function(loom_filepath) {
+    loom_sce_obj <- loom_to_sce(loom_filepath)
+    sparse_loom_matrix <- as(SummarizedExperiment::assay(loom_sce_obj), "dgCMatrix")
+    sparse_loom_matrix
+}
+
+#' @rdname sce
+#'
+#' @description function to change extract the existing sparse matrix
+#' representation (`dgRMatrix`) of the SingleCellExperiment object's assay
+#'
+#' @param h5ad_filepath character() absolute path to downloaded .h5ad file
+#'
+#' @return dgRMatrix sparse matrix representation
+.h5ad_sparse_matrix <- function(h5ad_filepath) {
+    h5ad_sce_obj <- h5ad_to_sce(h5ad_filepath)
+    sparse_h5ad_matrix <- SummarizedExperiment::assay(h5ad_sce_obj)
+    sparse_h5ad_matrix
+}
+
+#' @rdname sce
+#'
+#' @description convert a sparse matrix representation of the assay into a
+#' reformatted tibble
+#'
+#' @param sparse_matrix dgCMatrix sparse matrix representation
+#'
+#' @return tibble with row_index, col_index, and value of each non-zero entry
+#' in the assay matrix
+.loom_sparse_mtx_to_assay_tbl <- function(sparse_matrix) {
+    stopifnot(
+        ## sparse_matrix must inherit from class dgCMatrix
+        `'sparse_matrix =' must inherit from class 'dgCMatrix'` =
+            inherits(sparse_matrix, "dgCMatrix")
+    )
+
+    ## handling of sparse matrices depends on the initial file type
+    ## help: https://www.r-bloggers.com/2020/03/what-is-a-dgcmatrix-object-made-of-sparse-matrix-format-in-r/
+    ##dim(sparse_matrix)
+    ##length(sparse_matrix@i)
+    ##length(sparse_matrix@p)
+    ##length(sparse_matrix@x)
+
+    ## increment each element of row_index by one to match R's 1-based indexing
+    row_index <- sparse_matrix@i + 1
+    values <- sparse_matrix@x
+
+    ## diff between each consecutive integer in the sequence
+    ## sparse_matrix@p gives number of non-zero values in each column
+    ## as the sequence is made by reading the matrix
+    ## bottom to top, then left to right
+
+    loom_n_per_column <-  diff(sparse_matrix@p)
+    col_index <-  rep(seq_along(loom_n_per_column), loom_n_per_column)
+    ##str(loom_col_index)
+
+    # tibble is the row, column, and value for the entire matrix
+    loom_assay_tbl <- tibble(row_index, col_index, values)
+    loom_assay_tbl
+}
+
+#' @rdname sce
+#'
+#' @description convert a sparse matrix representation of the assay into a
+#' reformatted tibble
+#'
+#' @param sparse_matrix dgRMatrix sparse matrix representation
+#'
+#' @return tibble with row_index, col_index, and value of each non-zero entry
+#' in the assay matrix
+.h5ad_sparse_mtx_to_assay_tbl <- function(sparse_matrix) {
+    stopifnot(
+        ## sparse_matrix must inherit from class dgRMatrix
+        `'sparse_matrix =' must inherit from class 'dgRMatrix'` =
+            inherits(sparse_matrix, "dgRMatrix")
+    )
+
+    ## handling of sparse matrices depends on the initial file type
+    ## same as with .loom matrices, just compressed row wise vs column wise
+    ##dim(sparse_matrix)
+    ##length(sparse_matrix@j)
+    ##length(sparse_matrix@p)
+    ##length(sparse_matrix@x)
+
+    ## increment each element of col_index by one to match R's 1-based indexing
+    col_index <- sparse_matrix@j + 1
+    values <- sparse_matrix@x
+
+    ## diff between each consecutive integer in the sequence
+    ## sparse_matrix@p gives number of non-zero values in each row
+
+    h5ad_n_per_column <-  diff(sparse_matrix@p)
+    row_index <-  rep(seq_along(h5ad_n_per_column), h5ad_n_per_column)
+    ##str(h5ad_col_index)
+
+    # tibble is the row, column, and value for the entire matrix
+    h5ad_assay_tbl <- tibble(row_index, col_index, values)
+    h5ad_assay_tbl
+}
+
+#' @rdname sce
+#'
+#' @description function to change SingleCellExperiment or
+#' SingleCellLoomExperiment object's `rowData` into a gene annotation table
+#'
+#' @param sce SingleCellExperiment or SingleCellLoomExperiment object
+#'
+#' @return tibble of gene annotations
+.sce_rowdata_to_gene_tbl <- function(sce) {
+    stopifnot(
+        ## sce must be a SingleCellExperiment object
+        `'sce =' must inherit from class 'SingleCellExperiment'` =
+        inherits(sce, "SingleCellExperiment")
+    )
+
+    ## add row_index as a column
+    gene_tbl <- dplyr::as_tibble(rowData(sce))
+    gene_tbl %>% tibble::add_column(row_index = 1:nrow(gene_tbl),
+                            .before = colnames(gene_tbl)[1])
+    gene_tbl
+}
+
+#' @rdname sce
+#'
+#' @description function to change SingleCellExperiment or
+#' SingleCellLoomExperiment object's `colData` into a cell annotation table
+#'
+#' @param sce SingleCellExperiment or SingleCellLoomExperiment object
+#'
+#' @return tibble of cell annotations
+.sce_coldata_to_cell_tbl <- function(sce) {
+    stopifnot(
+        ## sce must be a SingleCellExperiment object
+        `'sce =' must inherit from class 'SingleCellExperiment'` =
+            inherits(sce, "SingleCellExperiment")
+    )
+
+    ## add col_index as a column
+    cell_tbl <- dplyr::as_tibble(colData(sce))
+    cell_tbl %>% tibble::add_column(col_index = 1:nrow(cell_tbl),
+                                    .before = colnames(cell_tbl)[1])
+    cell_tbl
+}
